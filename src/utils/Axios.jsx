67cf391e68d8d4a12c11b69a6dac2b34b1cb04fb
saroj <sarojpradhan7655@gmail.com>
2025-04-07 02:25:24 +0530

    changing baseURL

--- src/utils/Axios.jsx
+++ src/utils/Axios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-//  baseURL : 'https://quizbackend-jzmh.onrender.com/'
-  baseURL:'http://localhost:3000/'
+ baseURL : 'https://quizbackend-jzmh.onrender.com/'
+  // baseURL:'http://localhost:3000/'
 })
 export default instance
