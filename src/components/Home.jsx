0354ce1f5b1ae711a630ab829e1f37f6e8956b93
saroj <sarojpradhan7655@gmail.com>
2025-04-07 22:45:19 +0530

    adding 2nd section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,6 +19,7 @@ function Home(){
   },[])
   if(loading){return <Loader/>}
 return (
+  <div>
 <div className=' bg-white   w-full  flex flex-wrap justify-center'>
     <div className='min-w-[48%] p-8 md:mt-11 flex flex-col justify-start'>
            <div className=' mb-2  '>
@@ -32,6 +33,24 @@ return (
       </div>
   <div className='  sm:min-w-[44%] p-3'>
     <img className='h-[30rem] w-full' src=".\online-poll-concept-woman-with-questionnaire-internet-young-girl-choose-options-feedback_1002658-2663.jpg" alt="" srcset="" />
+  </div>
+</div>
+
+
+
+
+  <div className='grid sm:grid-cols-2 mt-6 mb-5'>
+  <div className=" h-fit p-5 m-6 text-5xl">
+    <div className='bg-black w-9 h-9 rotate-45 text-white mb-1.5'>@</div>
+  <p  className=''> Start your journey now,</p>
+ <p>level up your</p>
+<p>skills.</p>
+<p className='text-2xl mt-3'>Sarojpradhan76552gmail.com</p>
+</div>
+  <div className="font-serif text-gray-400 h-fit p-5 m-6 text-5xl">Talent is cheaper than salt,
+what separates a talented
+individual from the succesful
+one is a lot of hardwork</div>
   </div>
 </div>
 )
